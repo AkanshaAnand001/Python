1ad566e1d9f2f5f9b2f39bad8e162975991fef52
unknown <akansha.anand@computronicsonline.com>
2024-04-26 20:59:44 +0530

    second commit

---
+++ simpleinterest.py
@@ -0,0 +1,12 @@
+
+def simpleinterest():
+    P=float(input("Enter Principal:" ))
+    R=float(input("Enter Rate of Interest:" ))
+    T=float(input("Enter TimePeriod in years:" ))
+    SI=(P*R*T)/100
+    return SI
+print("Simple Interest is", simpleinterest())
+
+
+
+
