91f916abcf98e9113294ee8d656cb74fa182e413
unknown <akansha.anand@computronicsonline.com>
2024-04-26 21:13:01 +0530

    fifth commit- sum.py

---
+++ sum.py
@@ -0,0 +1,26 @@
+
+def Sum(a,b):
+    sum=float(a) + float(b)
+    return sum
+n1=input("first number:")
+n2=input("Second number:")
+print(Sum(n1,n2))
+print("The sum of {0} and {1} is {2}".format(n1,n2,Sum(n1,n2)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
